d22452742fa704a3ff3dd5b5ebd2503e6b67163e
Alexandre Sousa Dias Junior <jrdiiaas@icloud.com>
2023-12-27 12:17:03 -0300

    Inclusão do botão limpar

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ function enviarTexto() {
     document.getElementById('inputText').value = '';
 }
 
+function limparTexto() {
+    // Limpa o elemento de saída
+    document.getElementById('outputText').innerText = '';
+}
+
 // Adiciona um ouvinte de eventos ao campo de entrada para detectar alterações
 document.getElementById('inputText').addEventListener('input', function () {
     // Pode adicionar funcionalidades adicionais aqui se necessário
