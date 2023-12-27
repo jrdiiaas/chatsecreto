a3cd804c8e669e68ed064116432c01213904cbb8
Alexandre Sousa Dias Junior <jrdiiaas@icloud.com>
2023-12-27 14:45:38 -0300

    Alteração no CSS, HTML e JS para adicionar a funcionalidade de gerar novo parágrafo sempre que houver nova entrada e incluir a ação de texto secreto nos textos inputados, assim como adicionar a função de revelar texto quando o mouse sobrepor uma palavra.

--- script.js
+++ script.js
@@ -1,20 +1,58 @@
 function enviarTexto() {
-    // Obtém o valor do campo de entrada
-    var inputValue = document.getElementById('inputText').value;
+    const inputText = document.getElementById('inputText');
+    const outputText = document.getElementById('outputText');
 
-    // Atualiza o conteúdo do elemento de saída
-    document.getElementById('outputText').innerText = inputValue;
+    const texto = inputText.value.trim();
 
-    // Limpa o campo de entrada
-    document.getElementById('inputText').value = '';
+    if (texto !== '') {
+        // Criar um novo parágrafo
+        const paragrafo = document.createElement('p');
+
+        // Dividir o texto em palavras
+        const palavras = texto.split(' ');
+
+        // Para cada palavra, criar um elemento de span com a classe 'word'
+        palavras.forEach((palavra) => {
+            const span = document.createElement('span');
+            span.textContent = palavra;
+            span.classList.add('word');
+            paragrafo.appendChild(span);
+            // Adicionar um espaço entre as palavras
+            paragrafo.appendChild(document.createTextNode(' '));
+        });
+
+        // Adicionar o novo parágrafo ao output
+        outputText.appendChild(paragrafo);
+    }
+
+    // Limpar o input
+    inputText.value = '';
 }
 
-function limparTexto() {
-    // Limpa o elemento de saída
-    document.getElementById('outputText').innerText = '';
-}
+// Envolve o código em um evento de carregamento da página
+document.addEventListener('DOMContentLoaded', function () {
+    const outputText = document.getElementById('outputText');
 
-// Adiciona um ouvinte de eventos ao campo de entrada para detectar alterações
-document.getElementById('inputText').addEventListener('input', function () {
-    // Pode adicionar funcionalidades adicionais aqui se necessário
+    // Verifica se o elemento 'outputText' existe antes de adicionar os event listeners
+    if (outputText) {
+        outputText.addEventListener('mouseover', function (event) {
+            const target = event.target;
+
+            // Verifica se o elemento sobre o qual o mouse passou é uma palavra com a classe 'word'
+            if (target.classList.contains('word')) {
+                // Remove a classe 'word' quando o mouse passa sobre a palavra
+                target.classList.remove('word');
+            }
+        });
+
+        outputText.addEventListener('mouseout', function (event) {
+            const target = event.target;
+
+            // Verifica se o mouse saiu do elemento que era uma palavra
+            if (target.classList.contains('word')) {
+                // Adiciona a classe 'word' de volta quando o mouse sai da palavra
+                target.classList.add('word');
+            }
+        });
+    }
 });
